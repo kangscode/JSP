b83efe0a39889ab1f8b7beaaf923990c4759bede
kangscode <2009ksj@kau.kr>
2024-05-02 11:18:30 +0900

    joinColumn

--- ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -63,6 +65,30 @@ public class JpaMain {
             em.flush();*/
 
 //            System.out.println("===============");
+
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+//            member.setTeam(team); //**
+            em.persist(member);
+
+            team.addMember(member);
+
+//            team.getMembers().add(member);
+
+            em.flush();
+            em.clear();
+            
+            Member findMember = em.find(Member.class, member.getId());
+
+            List<Member> members = findMember.getTeam().getMembers();
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
